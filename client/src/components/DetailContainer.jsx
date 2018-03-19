fbe88095a20781a8befffa272385dc218586b576
Vahram Jacobs <vahram.jacobs@gmail.com>
2018-03-19 15:56:01 -0700

    Styles add and cancel buttons

--- client/src/components/DetailContainer.jsx
+++ client/src/components/DetailContainer.jsx
@@ -1,11 +1,11 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 import "./styles/DetailContainer.css";
 
-const DetailContainer = (props)=>{
+const DetailContainer = props => {
 	return (
 		<div className="detail-container">
-		
 			<header>
 				<div className="h2Wrapper">
 					<h2>{`add phase`.toUpperCase()}</h2>
@@ -13,22 +13,49 @@ const DetailContainer = (props)=>{
 			</header>
 			<div className="container-body">
 				<form>
-					<label htmlFor="summary">
-						<span>Summary</span>
-						<textarea type="text" id="summary" name="summary"/>
-					</label>
-					<br/>
-					<label htmlFor="name" > 
-						<span> Name for phase </span> 
-						<input id="name" type="text" name="name" />
-					</label>
-					<br/>
-					<button> add </button>
-					<a href="">cancel</a>
+					<fieldset>
+						<label htmlFor="name">
+							<p> Name for phase </p>
+							<input type="text" id="name" name="name" />
+						</label>
+					</fieldset>
+
+					<fieldset>
+						<label htmlFor="summary">
+							<p>Summary</p>
+							<textarea type="text" id="summary" name="summary" cols="60" rows="10" />
+						</label>
+					</fieldset>
+
+					<fieldset>
+						<label htmlFor="file">
+								<p>select image</p>
+								<input type="file" id="imageFile" name="imageFile" />
+						</label>
+
+{ /*
+						<label htmlFor="image-url">
+							<fieldset>
+								<p>
+									<input type="radio" name="imageInput" />
+									<span>Image url</span>
+								</p>
+								<input type="text" id="image-url" size="60" name="imageUrl" />
+							</fieldset>
+						</label>
+*/ }
+					</fieldset>
+
+					<fieldset>
+						<div className="btns">
+							<button className="btn btn-primary"> add </button>						
+							<Link className="btn btn-danger" to="/">cancel</Link>
+						</div>
+					</fieldset>
 				</form>
 			</div>
 		</div>
-	)
-}
+	);
+};
 
 export default DetailContainer;
