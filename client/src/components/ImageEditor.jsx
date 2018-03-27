21ebc802ef12b676694ca9fa4a389f5ef139fa15
Vahram Jacobs <vahram.jacobs@gmail.com>
2018-03-26 22:52:31 -0700

    Positon image editor's frame in the center of it's container

--- client/src/components/ImageEditor.jsx
+++ client/src/components/ImageEditor.jsx
@@ -6,16 +6,17 @@ import "./styles/ImageEditor.css";
 const parentWidth = 400;
 const parentHeight = 400;
 
-class ImageEditor extends Component{
-	constructor(props){
+class ImageEditor extends Component {
+	constructor(props) {
 		super(props);
 		console.log(this);
 	}
 
-	render(){
+	render() {
 		const { imageUrl, frameWidth, frameHeight } = this.props;
 
-		const image = <img
+		const image = (
+			<img
 				// style={{ display: this.state.loaded ? "block" : "none" }}
 				src={imageUrl}
 				alt="thumbnail preview for asset"
@@ -23,20 +24,24 @@ class ImageEditor extends Component{
 				// 	this.setState({ loaded: true });
 				// }}
 			/>
-		// vahram, when done, break this up to pop-up component & image-editor component
-		return(
+		);
+		const frameStyle = {
+			width: frameWidth,
+			height: frameHeight,
+			left: (parentWidth - frameWidth) / 2,
+			top: (parentHeight - frameHeight) / 2,
+		};
+		const wholeImageFrameStyle = {
+			left: (parentWidth - frameWidth) / 2,
+			top: (parentHeight - frameHeight) / 2,
+		};
+		return (
 			<div className="image-editor">
-				<div className="uncropped-image-frame">
+				<div className="whole-image-frame" style={wholeImageFrameStyle}>
 					{image}
 				</div>
 				<div className="unused-image-tint" />
-				<div
-					className="image-preview-frame"
-					style={{
-						width: frameWidth,
-						height: frameHeight
-					}}
-				>
+				<div className="image-preview-frame" style={frameStyle}>
 					{image}
 				</div>
 			</div>
@@ -48,10 +53,10 @@ ImageEditor.propTypes = {
 	imageUrl: string,
 	frameHeight: number.isRequired,
 	frameWidth: number.isRequired
-}
+};
 
 ImageEditor.defaultProps = {
 	imageUrl: ""
-}
+};
 
 export default ImageEditor;
