00a3541e2869e3802d282df9b4c35ae729126e35
Vahram Jacobs <vahram.jacobs@gmail.com>
2018-04-22 15:27:59 -0700

    Converts uploadImage utility function to use axios with formData instead of superagent

--- client/src/utils/uploadImage.js
+++ client/src/utils/uploadImage.js
@@ -1,75 +1,30 @@
-import sha1 from "sha1";
-import request from "superagent";
 import axios from "axios";
 
-// vahram, when all is working try this with axios
-// vahram, when all is working try this w/ regular file loader
 async function uploadFile (imageFile) {
+	// console.log("file: ", imageFile);
 
 	const cloudName = "cldimgs";
 	const url = `https://api.cloudinary.com/v1_1/${cloudName}/image/upload`;
 
 	const timestamp = Date.now() / 1000;
 	const uploadPreset = "affevzry";
-	const secret = "1IA49RibTzlQ-NmpNFt9i7LYVz0";
+	const apiKey = "295886862954169";
 
-	const paramsStr = `timestamp=${timestamp}&upload_preset=${uploadPreset}${secret}`;
-	const signature = sha1(paramsStr);
-	console.log("paramsStr: ", paramsStr);
-	console.log("signature: ", signature);
+	const formData = new FormData();
+	formData.append("file", imageFile);
+	formData.append("upload_preset", uploadPreset);
+	formData.append("api_key", apiKey);
+	formData.append("timestamp", timestamp);
 
-	const params = {
-		timestamp,
-		signature,
-		api_key: "295886862954169",
-		upload_preset: uploadPreset
-	};
-
-	const uploadProm = request
-		.post(url)
-		.attach("file", imageFile);
-
-	Object.keys(params).forEach(key => {
-		uploadProm.field(key, params[key]);
-	});
-
-	const reqResult = await uploadProm;
-
-	return reqResult.body.secure_url;
-
-	// console.log("file: ", imageFile);
-
-	// const cloudName = "cldimgs";
-	// const url = `https://api.cloudinary.com/v1_1/${cloudName}/image/upload`;
-
-	// const timestamp = Date.now() / 1000;
-	// const uploadPreset = "affevzry";
-	// const secret = "1IA49RibTzlQ-NmpNFt9i7LYVz0";
-
-	// const paramsStr = `timestamp=${timestamp}&upload_preset=${uploadPreset}${secret}`;
-	// const signature = sha1(paramsStr);
-	// console.log("paramsStr: ", paramsStr);
-	// console.log("signature: ", signature);
-
-	// const config = {
-	// 	timestamp,
-	// 	signature,
-	// 	api_key: "295886862954169",
-	// 	upload_preset: uploadPreset,
-	// 	method: "POST",
-	// 	data: {file: imageFile},
-	// 	url
-	// };
-
-	// try {
-	// 	const reqResult = await axios(config);
-	// 	return reqResult.body.secure_url;
-	// }
-	// catch(err){
-	// 	console.log(err);
-	// 	return null;
-	// }
-
+	// **** with axios
+	try {
+		const reqResult = await axios.post(url, formData);
+		return reqResult.data.secure_url;
+	}
+	catch(err){
+		console.log(err);
+		return null;
+	}
 };
 
 export default uploadFile;
