4f960faabd574e82e6cc031b1a929ae2014852cd
Vahram Jacobs <vahram.jacobs@gmail.com>
2019-08-07 00:17:54 -0700

    Changes server port

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 // require('./config/config.js');
 const app = require('./app');
 
-const port = process.env.PORT || 1234;
+const port = process.env.PORT || 12345;
 console.log('NODE_ENV: ', process.env.NODE_ENV);
 app.listen(port, () => {
 	console.log(`server is now listening on port ${port}`);
