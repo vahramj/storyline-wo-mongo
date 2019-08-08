a7295da90499276ebed8a28e028ca5040e731cfc
Vahram Jacobs <vahram.jacobs@gmail.com>
2019-08-08 09:29:37 -0700

    Adds dotenv package

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 // require('./config/config.js');
+require('dotenv').config();
 const app = require('./app');
 
 const port = process.env.PORT || 12345;
@@ -6,3 +7,4 @@ console.log('NODE_ENV: ', process.env.NODE_ENV);
 app.listen(port, () => {
 	console.log(`server is now listening on port ${port}`);
 });
+console.log(process.env.DB_HOST);
