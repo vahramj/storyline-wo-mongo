928e753ba18830807e3ab1fd7e4a0d5813b2adad
Vahram Jacobs <vahram.jacobs@gmail.com>
2019-08-08 21:25:31 -0700

    Changes to the port

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@
 require('dotenv').config();
 const app = require('./app');
 
-const port = process.env.PORT || 12345;
+const port = process.env.PORT || 1234;
 console.log('NODE_ENV: ', process.env.NODE_ENV);
 app.listen(port, () => {
 	console.log(`server is now listening on port ${port}`);
